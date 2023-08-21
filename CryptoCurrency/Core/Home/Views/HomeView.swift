56269dd74078c4a4b08355348c02ff3e31dbd16f
Charlie Nguyen <charlie.nguyen@getchip.uk>
2023-08-21 14:26:10 +0100

    Top Moving Coins UI

--- CryptoCurrency/Core/Home/Views/HomeView.swift
+++ CryptoCurrency/Core/Home/Views/HomeView.swift
@@ -2,7 +2,12 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                TopMoversView()
+            }
+            .navigationTitle("Live Prices")
+        }
     }
 }
 
