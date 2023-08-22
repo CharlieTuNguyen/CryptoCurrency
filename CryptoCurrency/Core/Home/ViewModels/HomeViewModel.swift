a576a2762f92a25899dccd263f73aea2b46d5ef5
Charlie Nguyen <charlie.nguyen@getchip.uk>
2023-08-22 09:46:33 +0100

    Model Setup & API Request

---
+++ CryptoCurrency/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+class HomeViewModel: ObservableObject {
+    
+    init() {
+        fetchCoinData()
+    }
+    
+    func fetchCoinData() {
+        let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h&locale=en"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("DEBUG: Error \(error.localizedDescription)")
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("DEBUG: Response code \(response.statusCode)")
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let coins = try decoder.decode([Coin].self, from: data)
+                print("DEBUG: Coins \(coins)")
+            } catch {
+                print("DEBUG: Failed to decode with error: \(error)")
+            }
+        }
+        .resume()
+    }
+}
---
+++ CryptoCurrency/Models/Coin.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+// MARK: - Coin
+struct Coin: Codable {
+    let id, symbol, name: String
+    let image: String
+    let currentPrice: Double
+    let priceChange24H, priceChangePercentage24H: Double
+    let marketCap, marketCapRank, fullyDilutedValuation, totalVolume: Double?
+    let high24H, low24H: Double
+    let marketCapChange24H, marketCapChangePercentage24H: Double?
+    let circulatingSupply, totalSupply: Double?
+    let maxSupply: Double?
+    let ath, athChangePercentage: Double?
+    let athDate: String?
+    let atlDate: String?
+    let lastUpdated: String?
+    let sparklineIn7D: SparklineIn7D?
+    let priceChangePercentage24HInCurrency: Double?
+}
+
+// MARK: - SparklineIn7D
+struct SparklineIn7D: Codable {
+    let price: [Double]
+}
