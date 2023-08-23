0a92ca6f30a3fa9805c8a861cdfdb39e9985506c
Charlie Nguyen <charlie.nguyen@getchip.uk>
2023-08-23 17:45:49 +0100

    number formatting

---
+++ CryptoCurrency/Extensions/Double.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+extension Double {
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.locale = Locale(identifier: "en-GB")
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    private var numberFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    func toCurrency() -> String {
+        return currencyFormatter.string(for: self) ?? "£0.00"
+    }
+    
+    func toPercentString() -> String {
+        guard let numberAsString = numberFormatter.string(for: self) else { return "" }
+        return numberAsString + "%"
+    }
+}
